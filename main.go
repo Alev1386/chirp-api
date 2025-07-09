0b2c36a009ebbf590a1f0a1ecf792b36241017d9
Alexander Yevstifeev <evstifeev2005@yandex.ru>
2025-07-09 11:34:34 +0300

    Added chirps and their configurations

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
+
 import (
     "encoding/json"
     "net/http"
 )
+
+type ChirpParam struct {
+    ID       int     `json:"id"`
+    StartHz  float64 `json:"start_hz"`
+    EndHz    float64 `json:"end_hz"`
+    Duration float64 `json:"duration_us"`
+}
+
+var chirps = []ChirpParam{
+    {ID: 1, StartHz: 100e6, EndHz: 200e6, Duration: 10},
+    {ID: 2, StartHz: 150e6, EndHz: 250e6, Duration: 15},
+}
+
+func chirpsHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(chirps)
+}
+
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
@@ -12,5 +31,6 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/health", healthHandler)
+    http.HandleFunc("/chirps", chirpsHandler)
     http.ListenAndServe(":8080", nil)
 }
